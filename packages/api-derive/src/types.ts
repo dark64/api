623d934145ffe3bdfbea571a196faa75f09d679b
Krzysztof Jelski <krzysztof@ethworks.io>
2020-12-03 16:28:19 +0100

    wip

--- packages/api-derive/src/types.ts
+++ packages/api-derive/src/types.ts
@@ -2,7 +2,27 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import type BN from 'bn.js';
-import type { AccountId, Balance, BalanceLock, BalanceLockTo212, BalanceOf, Bid, BidKind, BlockNumber, Hash, Index, Proposal, ProposalIndex, SetIndex, SocietyVote, StrikeCount, TreasuryProposal, Votes, VouchingStatus } from '@polkadot/types/interfaces';
+import type {
+  AccountId,
+  Balance,
+  BalanceLock,
+  BalanceLockTo212,
+  BalanceOf,
+  Bid,
+  BidKind,
+  BlockNumber,
+  Bounty,
+  Hash,
+  Index,
+  Proposal,
+  ProposalIndex,
+  SetIndex,
+  SocietyVote,
+  StrikeCount,
+  TreasuryProposal,
+  Votes,
+  VouchingStatus
+} from '@polkadot/types/interfaces';
 import type { u32 } from '@polkadot/types';
 
 export * from './accounts/types';
@@ -127,3 +147,8 @@ export interface VoterPosition {
 }
 
 export type DeriveVoterPositions = Record<string, VoterPosition>;
+
+
+export interface DeriveBounties {
+  bounties: Bounty[]
+}
